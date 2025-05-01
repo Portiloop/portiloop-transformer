b85e5a6f7e4af1e21e3b64ee815515f3616cd61d
Julien Stempfer <stempferjulien@gmail.com>
2025-05-01 17:27:02 -0400

    Added docstring for function, start work on class

--- transformiloop/src/data/pretraining.py
+++ transformiloop/src/data/pretraining.py
@@ -4,12 +4,20 @@ import os
 
 import pyedflib
 import torch
+from numpy import ndarray
 from torch.utils.data import Dataset
 
 
-def read_patient_info(dataset_path):
+def read_patients_info(dataset_path:str)->dict[str,dict[str,int|str|ndarray]]:
     """
     Read the patient info from a patient_info file and initialize it in a dictionary
+
+    Args:
+        dataset_path (str): Path to the dataset directory (NOT THE INFO FILE).
+
+    Returns:
+        dict[str,dict[str, int|str|ndarray]]: Dictionary containing the patient info (age, gender) for each subject in the dataset.
+
     """
     patient_info_file = os.path.join(dataset_path, 'patient_info.csv')
     with open(patient_info_file, 'r') as patient_info_f:
@@ -18,37 +26,44 @@ def read_patient_info(dataset_path):
         patient_info_f.seek(0)  # Rewind.
         reader = csv.reader(patient_info_f)
         if has_header:
-            next(reader)  # Skip header row.
+            next(reader)  # Skip the header row.
 
-        patient_info = {
+        patients_info = {
             line[0]: {
                 'age': int(line[1]),
                 'gender': line[2]
             } for line in reader
         }
-    return patient_info
+    return patients_info
 
 
-def read_pretraining_dataset(dataset_path, patients_to_keep=None):
+def read_pretraining_dataset(dataset_path:str, patients_to_keep:list[str]=None)->dict[str,dict[str,int|str|ndarray]]:
     """
     Load all dataset files into a dictionary to be ready for a Pytorch Dataset.
     Note that this will only read the first signal even if the EDF file contains more.
-    """
-    patient_info = read_patient_info(dataset_path)
 
-    for patient_id in patient_info.keys():
+    Args:
+        dataset_path (str): Path to the dataset directory.
+        patients_to_keep (list[str]): List of patient IDs to keep in the dataset. If None, all patients are kept.
+
+    Returns:
+        dict[str,dict[str, int|str|ndarray]]: Dictionary containing the patient info (age, gender) for each subject in the dataset.
+    """
+    patients_info = read_patients_info(dataset_path)
+
+    for patient_id in patients_info.keys():
         if patients_to_keep is not None and patient_id not in patients_to_keep:
             continue
         filename = os.path.join(dataset_path, patient_id + ".edf")
         try:
             with pyedflib.EdfReader(filename) as edf_file:
-                patient_info[patient_id]['signal'] = edf_file.readSignal(0)
+                patients_info[patient_id]['signal'] = edf_file.readSignal(0)
         except FileNotFoundError:
             logging.debug(f"Skipping file {filename} as it is not in dataset.")
 
-    # Remove all patients whose signal is not in dataset
-    dataset = {patient_id: patient_details for (patient_id, patient_details) in patient_info.items()
-        if 'signal' in patient_info[patient_id].keys()}
+    # Remove all patients whose signal is not in the dataset
+    dataset = {patient_id: patient_details for (patient_id, patient_details) in patients_info.items()
+        if 'signal' in patients_info[patient_id].keys()}
 
     return dataset
 
